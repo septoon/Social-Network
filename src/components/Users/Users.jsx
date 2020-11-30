9253aa05e24b7dd1e5c6fa25a3a6a39b74d417dd
Tigran <41443306+septoon@users.noreply.github.com>
2020-11-30 20:31:11 +0200

    51 lesson (Users API / axios)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import classes from './Users.module.css'
+import * as axios from 'axios'
+import userPhoto from '../../assets/images/user.jpg'
 
 const Users = (props) => {
+    if(props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items)
+        })
+    }
+
     return(
         <div className={classes.users_wrapper} >
             {props.users.map(u => <div key={u.id} className={classes.user_item}>
                 <div className={classes.left} >
                     <div>
-                        <img src={u.avatar} alt='avatar' className={classes.avatar}/>
+                        <img src={ u.photos.small !== null ? u.photos.small : userPhoto} alt='avatar' className={classes.avatar}/>
                     </div>
                     <div>
                         {u.followed
@@ -17,12 +25,12 @@ const Users = (props) => {
                 </div>
                 <div className={classes.right} >
                     <div className={classes.name} >
-                        <div className={classes.fullName} >{u.fullName}</div>
+                        <div className={classes.fullName} >{u.name}</div>
                         <div className={classes.status} >{u.status} </div>
                     </div>
                     <div className={classes.location} >
-                        <div className={classes.country} >{u.location.country}</div>
-                        <div className={classes.city} >{u.location.city}</div>
+                        <div className={classes.country} >{'u.location.country'}</div>
+                        <div className={classes.city} >{'u.location.city'}</div>
                     </div>
                 </div>
             </div>)}
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,10 +4,10 @@ const SET_USERS = 'SET_USERS'
 
 let initialState = {            
     usersData: [
-        {id: 1, avatar: 'https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg', followed: false, fullName: 'Tigran', status: 'I am here', location: {city: 'Kiev', country: 'Ukraine'}},
-        {id: 2, avatar: 'https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg', followed: false, fullName: 'Ivan', status: 'I am here', location: {city: 'Moscow', country: 'Russia'}},
-        {id: 3, avatar: 'https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg', followed: true, fullName: 'Dmitry', status: 'I am here', location: {city: 'Dnepr', country: 'Ukraine'}},
-        {id: 4, avatar: 'https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg', followed: true, fullName: 'Max', status: 'I am here', location: {city: 'Minsk', country: 'Belarus'}}
+        // {id: 1, avatar: 'https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg', followed: false, fullName: 'Tigran', status: 'I am here', location: {city: 'Kiev', country: 'Ukraine'}},
+        // {id: 2, avatar: 'https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg', followed: false, fullName: 'Ivan', status: 'I am here', location: {city: 'Moscow', country: 'Russia'}},
+        // {id: 3, avatar: 'https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg', followed: true, fullName: 'Dmitry', status: 'I am here', location: {city: 'Dnepr', country: 'Ukraine'}},
+        // {id: 4, avatar: 'https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg', followed: true, fullName: 'Max', status: 'I am here', location: {city: 'Minsk', country: 'Belarus'}}
     ]
 
 }   
