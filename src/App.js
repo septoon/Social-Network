8745f990f73da049e12408f2ad428e340a6627c9
Tigran <41443306+septoon@users.noreply.github.com>
2020-12-03 20:53:05 +0200

    60 lesson (withRouteer)

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = props => {
         <Navbar />
         <div className='app-wrapper-content'>
           <Route path='/dialogs' render={ () => <DialogsContainer /> } />
-          <Route path='/profile' render={ () => <ProfileContainer /> } />
+          <Route path='/profile/:userId?' render={ () => <ProfileContainer /> } />
           <Route path='/news' render={ () => <News /> } />
           <Route path='/music' render={ () => <Music /> } />
           <Route path='/users' render={ () => <UsersContainer /> } />
