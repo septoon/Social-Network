5a5e26b7fe21c2e1a52f60aafdf217e37d931d81
Tigran <41443306+septoon@users.noreply.github.com>
2020-12-27 21:00:20 +0200

    94 lesson (paginator? / react suspense)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import './App.css';
-import { Route, withRouter } from 'react-router-dom';
-import HeaderContainer from './components/Header/HeaderContainer';
-import Navbar from './components/Navbar/Navbar';
-import ProfileContainer from './components/Profile/ProfileContainer';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import News from './components/News/News';
-import Music from './components/Music/Music';
-import UsersContainer from './components/Users/UsersContainer';
-import Settings from './components/Settings/Settings';
-import Friends from './components/Friends/Friends';
-import Login from './components/Login/Login';
-import { connect } from 'react-redux';
+import React from 'react'
+import './App.css'
+import { Route } from 'react-router-dom'
+import HeaderContainer from './components/Header/HeaderContainer'
+import Navbar from './components/Navbar/Navbar'
+import News from './components/News/News'
+import Music from './components/Music/Music'
+import Settings from './components/Settings/Settings'
+import Friends from './components/Friends/Friends'
+import Login from './components/Login/Login'
+import { connect } from 'react-redux'
 import { initializeApp } from './redux/app-reducer'
-import Preloader from './components/common/Preloader/Preloader';
-import { BrowserRouter } from 'react-router-dom';
+import Preloader from './components/common/Preloader/Preloader'
+import { BrowserRouter } from 'react-router-dom'
 import store from './redux/redux-store'
-import {Provider} from 'react-redux';
+import {Provider} from 'react-redux'
+import { withSuspense } from './hoc/withSuspense'
 
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'))
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'))
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'))
 
 class App extends React.Component {
   componentDidMount() {
@@ -31,11 +32,11 @@ class App extends React.Component {
         <HeaderContainer />
         <Navbar />
         <div className='app-wrapper-content'>
-          <Route path='/dialogs' render={ () => <DialogsContainer /> } />
-          <Route path='/profile/:userId?' render={ () => <ProfileContainer /> } />
+          <Route path='/dialogs' render={ withSuspense(DialogsContainer) } />
+          <Route path='/profile/:userId?' render={ withSuspense(ProfileContainer) } />
           <Route path='/news' render={ () => <News /> } />
           <Route path='/music' render={ () => <Music /> } />
-          <Route path='/users' render={ () => <UsersContainer /> } />
+          <Route path='/users' render={ withSuspense(UsersContainer) } />
           <Route path='/settings' render={ () => <Settings /> } />
           <Route path='/friends' render={ () => <Friends /> } />
           <Route path='/login' render={ () => <Login /> } />
--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,19 +1,61 @@
-import React from 'react'
+// import React, { useState } from 'react'
+// import classes from './Paginator.module.css'
+
+// const Paginator = (props, portionSize = 10) => {
+//     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+//     let pages = []
+//     for(let i=1; i <= pagesCount; i++) pages.push(i)
+
+//     return(
+//         <div className={classes.pageNumbers} >
+//             { portionNumber > 1 ? <button onClick={ () => {setPortionNumber(portionNumber - 1)} } >Prev</button> : <button disabled >Prev</button> }
+//             {pages
+//             .filter( p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+//             .map((p) => {
+//                 if(pages.length > 30) pages.length = 30
+//                 return <span onClick={(e) => {props.onPageChanged(p)}}
+//                 className={props.currentPage === p && classes.selectedPage} 
+//                 >{p}</span>
+//             })}
+//             { portionCount > portionNumber ? <button onClick={ () => {setPortionNumber(portionNumber + 1)} } >Next</button> : <button disabled >Next</button> }
+//         </div>
+//     )
+// }
+
+// export default Paginator
+
+import React, { useState } from 'react'
 import classes from './Paginator.module.css'
+import cn from 'classnames'
 
 const Paginator = (props) => {
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
     let pages = []
     for(let i=1; i <= pagesCount; i++) pages.push(i)
-
+    let portionSize = 10
+    let portionCount = Math.ceil(pagesCount / portionSize)
+    let {portionNumber, setPortionNumber} = useState(1)
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
+    let rightPortionPageNumber = portionNumber * portionSize
+ debugger
     return(
         <div className={classes.pageNumbers} >
-            {pages.map((p) => {
+            { portionNumber > 1 ? <button onClick={ () => {setPortionNumber(portionNumber - 1)} } >Prev</button> : <button disabled >Prev</button> }
+            {pages
+            .filter( p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+            .map((p) => {
                 if(pages.length > 30) pages.length = 30
-                return <span onClick={(e) => {props.onPageChanged(p)}} className={props.currentPage === p && classes.selectedPage} >{p}</span>
+                return <span className={ cn({
+                    [classes.selectedPage]: props.currentPage === p
+                }, classes.pageNumber) }
+                key={p} 
+                onClick={ (e) => {
+                    props.onPageChanged(p)
+                }} >{p}</span>
             })}
+            { portionCount > portionNumber ? <button onClick={ () => {setPortionNumber(portionNumber + 1)} } >Next</button> : <button disabled >Next</button> }
         </div>
     )
 }
-
+ 
 export default Paginator
